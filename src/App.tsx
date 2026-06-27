80ef80ef59e0015c5e2c707709827b2c56185a6a
Gideon <ekibade.gideon@yahoo.comm>
2026-06-27 08:12:56 +0100

    feat: add Gallery page with filter tabs, image grid, CTA and FAQs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// <<<<<<< HEAD
+
+// import MainLayout from "./layouts/MainLayout";
+
+// =======
+// >>>>>>> 01866e5 (feat: add Gallery page with filter tabs, image grid, CTA and FAQs)
+// import Home from "./pages/Home";
+// import Contact from "./pages/Contact";
+// import Services from "./pages/Services";
+// import Gallery from "./pages/Gallery";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route element={<MainLayout />}>
+//           <Route path="/" element={<Home />} />
+
+//           <Route path="/services" element={<Services />} />
+
+// <<<<<<< HEAD
+//           <Route path="/gallery" element={<Gallery />} />
+
+//           <Route path="/contact" element={<Contact />} />
+//         </Route>
+// =======
+//         <Route path="/gallery" element={<Gallery />} />
+//         <Route path="/contact" element={<Contact />} />
+// >>>>>>> 01866e5 (feat: add Gallery page with filter tabs, image grid, CTA and FAQs)
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import MainLayout from "./layouts/MainLayout";
-
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
@@ -13,11 +47,8 @@ export default function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-
           <Route path="/services" element={<Services />} />
-
           <Route path="/gallery" element={<Gallery />} />
-
           <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
